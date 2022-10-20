a7941fcbaa2b4c3d6927f80b6f95d2d8029de0b3
Igor Candido <igorcr39@gmail.com>
2022-10-20 19:27:22 -0300

    corrected error

--- clientes/urls.py
+++ clientes/urls.py
@@ -1,10 +1,10 @@
 from django.urls import path
-from .views import people_list, person_new, home, person_update
+from .views import people_list, person_new, home, person_update, person_delete
 
 urlpatterns = [
     path('list/', people_list, name="person_list"),
     path('new/', person_new, name="person_new"),
     path('update/<int:id>/', person_update, name="person_update"),
-    path('delete/<int:id>/', person_update, name="person_delete"),
+    path('delete/<int:id>/', person_delete, name="person_delete"),
     path('home/', home),
 ]
