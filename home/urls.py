d9f21733bcff5d83a33dfcc86984ca989f81084a
Igor Candido <igorcr39@gmail.com>
2022-10-26 06:14:25 -0300

    added new files and functions on start studies of CBV

--- home/urls.py
+++ home/urls.py
@@ -1,8 +1,17 @@
 from django.urls import path
-from .views import home, my_logout
+from .views import home, my_logout, HomePageView, MyView
+from django.views.generic.base import TemplateView
 
 urlpatterns = [
     path('', home, name='home'),
     path('logout/', my_logout, name='my_logout'),
+
+    # CLASSES BASED VIEWS
+    path('view', MyView.as_view()),
+    # No caso abaixo, é um exemplo de CBV (Classes based views). Onde não é preciso criar uma view para apenas mostrar um html, o que conseguimos fazer com apenas uma classe chamada TemplateView. Ou seja, a TemplateView apenas mostra o html sem poder acrescentar informações no html
+    path('home2', TemplateView.as_view(template_name='home2.html')),
+    #Já esse exemplo, nos conseguimos acrescentar informações com uma fácil alterações de heranças de classes, customizando uma classe chamada HomePageView
+    path('home3', HomePageView.as_view(template_name='home3.html')),
+
 
 ]
--- home/views.py
+++ home/views.py
@@ -8,3 +8,30 @@ def my_logout(request):
     logout(request)
     return render(request, 'home.html')
 
+# CLASSES BASED VIEWS
+
+#Utilizando a classe mais simples, a View, nos conseguimos renderizar htmls e definir funções dentro da classe para cada método http
+from django.http import HttpResponse
+from django.views import View
+class MyView(View):
+    def get(self, request, *args, **kwargs):
+        #return HttpResponse('hello world')
+        return render(request, 'home3.html')
+    
+    def post(self, request, *args, **kwargs):
+        return HttpResponse('hello world feito em post')
+
+
+# No caso abaixo, é outro exemplo de CBV (Classes based views). Onde nos utilizamos o sistema de heraças de classes no python, para herdar atributos da classe chamada TemplateView, e configuramos nossa propria classe para home page chamada HomePageView
+from django.views.generic.base import TemplateView
+class HomePageView(TemplateView):
+    template_name = 'home3.html'
+
+
+    def get_context_data(self, **kwargs):
+        #O super() é utilizado entre heranças de classes, ele nos proporciona extender/subscrever métodos de uma super classe (classe pai) para uma sub classe (classe filha), atrávez dele definimos um novo comportamento para um determinado método construido na classe pai e herdado pela classe filha.
+        context = super().get_context_data(**kwargs)
+        context['testando'] = 'Adicionando valores por método de herança'
+        return context
+
+
