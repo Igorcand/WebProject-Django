6173aac2a33ebfb30351b8066ba677c053eea3da
Igor Candido <igorcr39@gmail.com>
2022-10-26 07:31:05 -0300

    added files end routes to use basic classes route to create, detail, list, update and delete

--- clientes/urls.py
+++ clientes/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import people_list, person_new, home, person_update, person_delete
+from .views import people_list, person_new, home, person_update, person_delete, PersonListView, PersonDetailView, PersonCreateView, PersonUpdateView, PersonDeleteView
 
 urlpatterns = [
     path('list/', people_list, name="person_list"),
@@ -7,4 +7,14 @@ urlpatterns = [
     path('update/<int:id>/', person_update, name="person_update"),
     path('delete/<int:id>/', person_delete, name="person_delete"),
     path('home/', home),
+    # CLASSES BASED VIEWS
+    path('person_list/', PersonListView.as_view(),name='person_list_cbv'),
+    path('person_detail/<int:pk>/', PersonDetailView.as_view(), name='person_detail_cbv'),
+    path('person_update/<int:pk>/', PersonUpdateView.as_view(), name='person_update_cbv'),
+    path('person_delete/<int:pk>/', PersonDeleteView.as_view(), name='person_delete_cbv'),
+
+    path('person_create/', PersonCreateView.as_view(), name='person_form_cbv'),
+    
+
+
 ]
--- clientes/views.py
+++ clientes/views.py
@@ -38,3 +38,51 @@ def person_delete(request, id):
         person.delete()
         return redirect('person_list')
     return render(request, 'person_delete_confirm.html', {'person':person})
+
+# CLASSES BASED VIEWS
+
+# ListView
+
+# A função herdada na classe chamada ListView, serve para fazer uma listagem dos valores que estão cadastrados no banco de dados, como se fosse, por baixo dos panos, 'Person.objects.all()', e ele adiciona em uma variável padrão chamada object_list. Feito isso, podemos apenas acessas a variável no template, adicionar no html
+from django.views.generic.list import ListView
+class PersonListView(ListView):
+    #Se não falarmos qual é o template name, o django irá procurar dentro da sua app, em templates, uma outra pasta com o mesmo nome da app, com o arquivo com o nome do model, todo em minusculo, + _list. Então o arquivo seria person_list.html, na raiz app_name/templates/app_name/modelname_list.html
+    model = Person
+
+# DetailView
+
+# Da mesma forma que o ListView pega todos os valores da tabela no banco por debaixo dos panos, o DetailView seria como um filter_by_id, pegando todas as informações pela primary key
+from django.views.generic.detail import DetailView
+class PersonDetailView(DetailView):
+    model = Person
+
+# CreateView
+#É uma view para criação de formularios
+from django.views.generic.edit import CreateView
+class PersonCreateView(CreateView):
+    model = Person
+    fields = ['first_name', 'last_name', 'age', 'salary', 'bio', 'photo']
+    success_url = '/clientes/person_list/'
+
+# UpdateView
+
+#É uma view para edição de formularios
+from django.views.generic.edit import UpdateView
+from django.urls import reverse_lazy
+class PersonUpdateView(UpdateView):
+    model = Person
+    fields = ['first_name', 'last_name', 'age', 'salary', 'bio', 'photo']
+    success_url = reverse_lazy('person_list_cbv')
+
+# DeleteView
+
+#É uma view para deletar valores
+from django.views.generic.edit import DeleteView
+from django.urls import reverse_lazy
+class PersonDeleteView(DeleteView):
+    model = Person
+    #success_url = reverse_lazy('person_list_cbv')
+
+    #Utilizando a função abaixo, nos podemos manipular o objeto de entrada, e depois redirecionamos, ao invés de só redirecionar
+    def get_success_url(self):
+        return reverse_lazy('person_list_cbv')
