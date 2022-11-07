7c8087346a8da381ec237b0cfa563efc572dea80
Igor Candido <igorcr39@gmail.com>
2022-11-07 06:17:52 -0300

    dashborad with models function

--- vendas/views.py
+++ vendas/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from django.views import View
+from django.db.models import Sum, F, FloatField, Max, Avg, Min, Count
+from .models import Sale
 
-# Create your views here.
+class DashboardView(View):
+    def get(self, request):
+        media = Sale.objects.all().aggregate(Avg('value'))['value__avg']
+        media_desc = Sale.objects.all().aggregate(Avg('discount'))['discount__avg']
+        min = Sale.objects.all().aggregate(Min('value'))['value__min']
+        max = Sale.objects.all().aggregate(Max('value'))['value__max']
+        qnt = Sale.objects.all().aggregate(Count('id'))['id__count']
+        qnt_nfe = Sale.objects.filter(nfe_emitida=True).aggregate(Count('id'))['id__count']
+
+        return render(request, 'vendas/dashboard.html', {'media':media, 'media_desc':media_desc, 'min': min, 'max': max, 'qnt':qnt, 'qnt_nfe': qnt_nfe})
